5b968e6892a2686719f62e9139a665d953186f72
ajay1478 <109083733+ajay1478@users.noreply.github.com>
2022-07-12 12:03:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,602 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (
+<div>
+
+   {/* start contact info */}
+ 
+ 
+     <div className={'bk'}> 
+     <div className={'container'}>
+  
+      <div className={'space'}>
+          <div>
+            <ul className={'sym'}>
+              <li>info@cdmi.in</li>
+              <li>verify certificate</li>
+            </ul>
+          </div>
+           <div>
+             <ul className={'sym'}>
+                <li>
+                   HAVE ANY QUESTION ? +91 90333 16003
+               </li>
+              </ul>
+           </div>
+        <div>
+          <ul className={'sym'}>
+            <li> social media icom</li>
+          </ul>
+        </div>
+
+
+      </div>
+      </div>
+    </div>
+  
+    
+      {/* end contact info */}
+
+
+    {/* start header */}
+
+      <div className={'container'}>
+        <div className={'inl'}>
+        <div >
+          <img src={require('./image/asset 0.jpg')} className={'logo'}></img>
+        </div>
+        <div>
+          <ul className={'tym'}>
+            <li>home </li>
+            <li>courses </li>
+            <li>collage courses</li>
+            <li>activities</li>
+            <li>blog </li>
+            <li>known us </li>
+            <li>get in touch </li>
+            <li>student zone </li>
+          </ul>
+        </div>
+        
+        </div>
+       </div>
+
+
+
+     {/* end header */}
+
+
+
+     {/* start image */}
+
+      <div>
+           <img src={require('./image/asset 3.jpeg')} className={'image'}></img>
+      </div>
+
+
+      {/* end image */}
+
+{/* start main */}
+ <main className={'sp'}>
+  <div className={'container'}>
+
+
+        <div className="main  ">
+              <span>creative course</span>
+              <h2>
+                offered courses
+              </h2>
+          </div>
+
+
+
+    <div className={'row'}>
+
+      <div className="single">
+        <div className="img1">
+        <img src={require('./image/asset 1.jpeg')} className={'img2'}></img>
+        </div>
+        <div className={'brd'}><h4  className={'mrg'}>Multimedia Courses</h4></div>
+        <div>
+          <ul className={'detail'}>
+            <li className={'fnt'}>*****</li>
+            <li className={'sty'}>know mopre..!</li>
+          </ul>
+        </div>
+
+        
+              
+      </div>
+      
+      <div className="single">
+        <div className="img1">
+        <img src={require('./image/asset 2.jpeg')} className={'img2'}></img>
+        </div>
+        <div className={'brd'}><h4  className="mrg">UI/UX & Graphics Design</h4></div>
+        <div>
+          <ul className="detail">
+            <li  className="fnt">*****</li>
+            <li className="sty">know mopre..!</li>
+          </ul>
+        </div>
+              
+      </div>
+      
+      <div className="single">
+        <div className="img1">
+        <img src={require('./image/asset 4.jpeg')} className={'img2'}></img>
+        </div>
+        <div className={'brd'}><h4 className="mrg">Programming IT</h4></div>
+        <div>
+          <ul className="detail">
+            <li  className="fnt">*****</li>
+            <li className="sty">know mopre..!</li>
+          </ul>
+        </div>
+              
+      </div>
+
+    </div>
+
+    <div className={'row2'}>
+
+<div className="single">
+  <div className="img1">
+  <img src={require('./image/asset 5.jpeg')} className={'img2'}></img>
+  </div>
+  <div className={'brd'}><h4  className={'mrg'}>Trendy Courses</h4></div>
+  <div >
+    <ul className={'detail'}>
+      <li className={'fnt'}>*****</li>
+      <li className={'sty'}>know mopre..!</li>
+    </ul>
+  </div>
+
+  
+        
+</div>
+
+<div className="single">
+  <div className="img1">
+  <img src={require('./image/asset 6.jpeg')} className={'img2'}></img>
+  </div>
+  <div className={'brd'}><h4  className="mrg">Civil-Mech. Engineering</h4></div>
+  <div >
+    <ul className="detail">
+      <li  className="fnt">*****</li>
+      <li className="sty">know mopre..!</li>
+    </ul>
+  </div>
+        
+</div>
+
+<div className="single">
+  <div className="img1">
+  <img src={require('./image/asset 7.jpeg')} className={'img2'}></img>
+  </div>
+  <div className={'brd'}><h4 className="mrg">Business Development</h4></div>
+  <div >
+    <ul className="detail">
+      <li  className="fnt">*****</li>
+      <li className="sty">know mopre..!</li>
+    </ul>
+  </div>
+        
+</div>
+
+
+
+</div>
+
+
+<div className={'bt'}>
+  <p className={'p'}>
+    view all courses
+  </p>
+</div>
+
+
+{/* end gallery */}
+
+</div>
+
+
+    <div className={'container bot'}>
+      <div className={'par'}>
+      <div className={'wi'}>
+     <p> ABOUT US</p>
+<p>IT CAREER IN SURAT, MOVING TOWARDS THE BETTER TOMORROW!</p>
+Creative Design & Multimedia Institute(CDMI) is a reputed training institute in Surat for web design, Game design,Mobile App Development, Game Development ,Video Editing & All types of IT Courses with 8 years of full-fledged, result-oriented training experience. We stepped in the market in 2014 with the goal to help students, working professionals and other interested candidates get that dream job or open that desired freelance business in some of the most popular Computer / IT fields. Our aim is to ease the hiring process for businesses and organizations by providing work-ready professionals who can contribute greatly to their success. Since then, we have worked hard to achieve this goal and dedicated our time and resources to train students extensively.
+
+
+<div className={'bt1'}>
+  <p className={'p'}>
+    enrol now..!
+  </p>
+</div>
+
+
+      </div>
+      
+      <div className={'wii'}>
+      <img src={require('./image/asset 11.jpeg')} className={'img3'}></img>
+      </div>
+      </div>
+      </div>
+    
+
+
+
+
+
+
+<div className={'sec'}> 
+
+<div className={'img4'}>
+{/* <img src={require('./image/asset 34.jpeg')} ></img> */}
+</div>
+
+<div className={'imgp container'}>
+  <div >
+    <p>
+      8000+<br></br>
+      happy students
+    </p>
+  </div>
+  <div>
+    <p>
+      10+<br></br>
+      certification approval
+    </p>
+  </div>
+  <div>
+    <p>
+      60+<br></br>
+      certified teacher
+    </p>
+  </div>
+  <div>
+    <p>
+      4000+<br></br>
+      student placed
+    </p>
+  </div>
+</div>
+
+</div>
+
+
+<div className={'container pad'}>
+      
+     
+      <div className="min">
+              <span>happy students</span>
+              <h2>
+                alumni speak<br></br><br></br>
+              </h2>
+          
+          <div>
+        
+        <p>
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc
+        </p>
+      
+        
+      </div>   
+      </div>   
+      <div className={'contain'}>
+      <img src={require('./image/asset 36.jpeg')} className={'img5'}></img>
+      </div>
+ </div>
+      
+    
+
+    <div className={'bak'}>
+
+   
+
+      <div className={'container'}>
+
+
+          <div className="main  ">
+              <span>read our defference</span>
+              <h2>
+                why choose creative
+              </h2>
+          </div>
+
+
+        <div>
+
+            <tr className="tr">
+              <td>
+              <img src={require('./image/asset 1.jpeg')} className={'img6'}></img>
+                <p>
+               <h3> Industry Experts As Trainers</h3><br></br>
+                  Our trainers have 5+ years of industry<br></br>
+                  experience coupled with extensive<br></br>
+                   research and analysis
+                </p>
+              </td>
+              <td>
+              
+              <img src={require('./image/asset 2.jpeg')} className={'img6'}></img>
+                <p>
+               <h3> Latest Curriculum</h3><br></br>
+                  Our trainers have 5+ years of industry<br></br>
+                  experience coupled with extensive<br></br>
+                   research and analysis
+                </p>
+
+              </td>
+              <td>
+
+                  
+              <img src={require('./image/asset 3.jpeg')} className={'img6'}></img>
+                <p>
+               <h3> Latest Technology</h3><br></br>
+                  Our trainers have 5+ years of industry<br></br>
+                  experience coupled with extensive<br></br>
+                   research and analysis
+                </p>
+              </td>
+
+
+            </tr>  
+
+            <tr className="tr">
+              <td>
+              <img src={require('./image/asset 10.jpeg')} className={'img6'}></img>
+                <p>
+               <h3> Interview Assistance</h3><br></br>
+                  Our trainers have 5+ years of industry<br></br>
+                  experience coupled with extensive<br></br>
+                   research and analysis
+                </p>
+              </td>
+              <td>
+              
+              <img src={require('./image/asset 8.jpeg')} className={'img6'}></img>
+                <p>
+               <h3> Free Upgradation</h3><br></br>
+                  Our trainers have 5+ years of industry<br></br>
+                  experience coupled with extensive<br></br>
+                   research and analysis
+                </p>
+
+              </td>
+              <td>
+
+                  
+              <img src={require('./image/asset 9.jpeg')} className={'img6'}></img>
+                <p>
+               <h3> Lifetime Support</h3><br></br>
+                  Our trainers have 5+ years of industry<br></br>
+                  experience coupled with extensive<br></br>
+                   research and analysis
+                </p>
+              </td>
+
+
+            </tr>  
+
+
+
+
+        </div>
+
+
+
+      </div>
+
+    </div>
+
+
+
+    <div className={'container'}>
+
+    <div className="main  ">
+              <span>student placement</span>
+              <h2>
+              OUR RECRUITMENT PARTNERS
+              </h2>
+          </div>
+
+        <div>
+          <ul className={'ul'}>
+            <li>
+            <img src={require('./image/asset 23.png')} ></img>
+            </li>
+            <li>
+            <img src={require('./image/asset 24.png')} ></img>
+            </li>
+            <li>
+            <img src={require('./image/asset 25.png')} ></img>
+            </li>
+            <li>
+            <img src={require('./image/asset 26.png')} ></img>
+            </li>
+            <li>
+            <img src={require('./image/asset 27.png')} ></img>
+            </li>
+            <li>
+            <img src={require('./image/asset 28.png')} ></img>
+            </li>
+          </ul>
+        </div>
+
+
+        <div>
+        <p className={'one'}><h2>Our Demanded Course -</h2></p>
+          <tr className={'sett'}>
+            <td>Game Design Course In Katargam</td>
+            <td>Advance Java Training Institute In Surat</td>
+            <td>Best It Course</td>
+            <td>CCC Certificate Center In Surat</td>
+            <td>Advance Java Training Course</td>
+            <td>Wordpress Training Course</td>
+            
+
+          </tr>
+          <tr className={'sett'}>
+            <td>C++ Programming Language Training Institute In Adajan</td>
+            <td>Advance Java Training Institute In Surat</td>
+            <td>Best It Course</td>
+            <td>CCC Certificate Center In Surat</td>
+            <td>Advance Java Training Course</td>
+            <td>Wordpress Training Course</td>
+            
+
+          </tr>
+        
+          <tr className={'sett'}>
+            <td>Game Design Course In Katargam</td>
+            <td>Advance Java Training Institute In Surat</td>
+            <td>Best It Course</td>
+            <td>CCC Certificate Center In Surat</td>
+            <td>Advance Java Training Course</td>
+            <td>Wordpress Training Course</td>
+            
+
+          </tr>
+        
+          <tr className={'sett'}>
+            <td>Game Design Course In Katargam</td>
+            <td>Advance Java Training Institute In Surat</td>
+            <td>Best It Course</td>
+            <td>CCC Certificate Center In Surat</td>
+            <td>Advance Java Training Course</td>
+            <td>Wordpress Training Course</td>
+            
+
+          </tr>
+
+          <p>see more</p>
+
+        
+        </div>
+
+
+  
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+      </main>
+
+
+<footer >
+
+<div className={'back'}>
+
+
+<div className={'container'}>
+  
+<div className={'ftsp'}>
+    <ul className={'spc'}>
+      <li>
+      <img src={require('./image/creative-logo-white.png')} className={'ftlogo'}></img>
+      </li>
+      <li>
+      Creative Design and Multimedia Institute <br></br>is leading computer training institute in <br>
+      </br>surat. We offers government approved <br></br>computer training courses in Surat.
+      </li>
+ 
+      <li>
+      FOLLOW US ON
+      <p>social media icon</p>
+
+      </li>
+    </ul>
+    <ul className={'spc'}>
+      <li>
+      FEATURE LINKS
+      </li>
+      <li>about us</li>
+      <li>blog</li>
+      <li>join us</li>
+      <li>company login</li>
+      <li>certificate verification</li>
+    </ul>
+    <ul className={'spc'}>
+      <li>
+      TRENDY COURSES
+      </li>
+      <li>Python course training</li>
+      <li>Node JS course training</li>
+      <li>Angular JS course training</li>
+      <li>React JS course training</li>
+      <li>Unity 3D course training</li>
+      <li>
+      Ethical Hacking course training
+      </li>
+      <li>React Native course training</li>
+      <li>Desktop App</li>
+      <li>Development course training</li>
+      <li>Advanced ASP.net</li>
+      <li>development course training</li>
+    </ul>
+
+    
+    <ul className={'spc'}>
+      <li>
+      OUR BRANCHES
+      </li>
+      <li>HEAD OFFICE - YOGICHOWK</li>
+      <li>401-404, 4th Floor, City Center Complex, Nr. Yogichowk, Varachha, Surat
+Mo : +91 90333 16003,</li>
+      <li>BRANCH 1 - KATARGAM</li>
+      <li>327-334, 3rd Floor, Laxmi Enclave, Opp. Gajera School, Katargam, Surat
+Mo : +91 90 3333 5009</li>
+      <li>BRANCH 2 - MOTA VARACHHA</li>
+      <li>B 201-203, 2nd Floor, Aditya Complex, VIP Circle, Utran
+Mota Varachha,Surat
+Mo : +91 945 945 9673</li>
+    </ul>
+
+
+</div>
+
+
+
+</div>
+
+</div>
+
+<div className={'bn'}>
+<div className={'container'}>
+<div >
+<p className={'lst'}>© 2022 All Rights Reserved by Creative Design & Multimedia Institute.</p>
+</div> 
+</div>
+</div>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+    
+</div>
+);
+}
+
+export default App;
